fd6ca6f8abb786e823cf39077d8487b5f84767fb
Basraah <basraah@users.noreply.github.com>
2017-09-19 08:27:02 +1000

    Update setup details

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     version='0.1.1',
     description=u"A python client for Openfire's REST API Plugin",
     license="GPL-3",
-    author='Sergey Fedotov (seamus-45)',
-    author_email='sr.fido@gmail.com',
-    url='https://github.com/seamus-45/openfire-restapi',
+    author='Alliance Auth, Sergey Fedotov (seamus-45)',
+    author_email='basraaheve+openfire-restapi@gmail.com',
+    url='https://github.com/allianceauth/openfire-restapi/',
     packages=['ofrestapi'],
     install_requires=['requests>=2.9.1'],
 )
